920f21e627b3028732131d8229adc08a56dc6f80
Thuan NN <thuan.nguyenngoc@live.com>
2017-08-05 15:58:44 +0700

    + proj

---
+++ rFDmgr/Service1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rFDmgr
+{
+    public partial class Service1 : ServiceBase
+    {
+        Server2 mServer;
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            mServer = new Server2(SrvrBufHndl);
+            mServer.SrvrPort = 23822;
+            Task.Run(() => mServer.Start());
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        public bool SrvrBufHndl(byte[] buf, out byte[] outMsg)
+        {
+            outMsg = null;
+            return false;
+        }
+    }
+}
